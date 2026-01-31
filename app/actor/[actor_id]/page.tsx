12b88e07f6b93f2f6884375e8a3047d213b693d4
wesam lafi <wesamlafi390@gmail.com>
2026-01-31 23:46:56 +0300

    Optimized the actor/actor-id route

--- app/actor/[actor_id]/page.tsx
+++ app/actor/[actor_id]/page.tsx
@@ -1,37 +1,34 @@
 import ActorHero from '@/app/components/ActorHero';
 import KnownForMovies from '@/app/components/KnownForMovies';
-
 import type { ActorDetails } from '@/app/types/actors';
 
 const page = async ({ params }: { params: { actor_id: string } }) => {
+  const { actor_id } = params;
 
-    const { actor_id } = await params
-    const res = await fetch(
-        `https://api.themoviedb.org/3/person/${actor_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US&append_to_response=combined_credits`,
-        {
-            next: { revalidate: 86400 }, // cache for 24 hours
-        }
-    )
-    if (!res.ok) {
-        throw new Error("No Actor found");
+  // Fetch actor data with ISR
+  const res = await fetch(
+    `https://api.themoviedb.org/3/person/${actor_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US&append_to_response=combined_credits`,
+    {
+      next: { revalidate: 86400 } // 24h cache
     }
+  );
 
-    const actor: ActorDetails = await res.json()
+  if (!res.ok) {
+    throw new Error('No Actor found');
+  }
 
+  const actor: ActorDetails = await res.json();
 
-    return (
-        <div className="relative ">
-            <div className="py-16 ">
-                <ActorHero actor={actor} />
-            </div>
-            <div className="pt-4">
-                <KnownForMovies cast={actor?.combined_credits?.cast ?? []} />
+  return (
+    <div className="relative">
+      <div className="py-16">
+        <ActorHero actor={actor} />
+      </div>
+      <div className="pt-4">
+        <KnownForMovies cast={actor?.combined_credits?.cast ?? []} />
+      </div>
+    </div>
+  );
+};
 
-            </div>
-
-
-        </div >
-    )
-}
-
-export default page
+export default page;
--- app/components/KnownForMovies.tsx
+++ app/components/KnownForMovies.tsx
@@ -1,51 +1,33 @@
-import type { KnownFor } from '../types/actors'
-import SectionTitle from './SectionTitle'
-import MovieCard from './MovieCard'
+import Link from 'next/link';
+import MovieCard from './MovieCard';
+import SectionTitle from './SectionTitle';
+import type { KnownFor } from '@/app/types/actors';
 
 const KnownForMovies = ({ cast }: { cast: KnownFor[] }) => {
+  if (!cast || cast.length === 0) return null;
 
+  let filteredCast = cast
+    .filter((c) => c.media_type === 'movie' && c.order === 0 && c.poster_path)
+    .slice(0, 10);
 
-    if (cast.length == 0) return
+  if (filteredCast.length <= 4) {
+    filteredCast = cast.filter((c) => c.media_type === 'movie').slice(0, 10);
+  }
 
-    let filteredCast = cast.filter((c) => {
-        if (c.media_type == "movie" && c.order == 0 && c.poster_path != null) {
-            return c
-        }
-       
-    }
-    ).slice(0, 10)
-    if (filteredCast.length <= 4) {
-        filteredCast = cast.filter((c) => c.media_type == "movie").slice(0, 10)
-    }
+  if (filteredCast.length === 0) return null;
 
+  return (
+    <div className="py-8">
+      <SectionTitle title="Known For:" />
+      <div className="flex overflow-x-auto gap-4 py-4">
+        {filteredCast.map((movie, i) => (
+          <div className="w-[200px] min-w-[200px]" key={i}>
+            <MovieCard movieData={movie} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-
-    return (
-
-        <div className="py-8">
-            {filteredCast.length != 0 &&
-                <div className="">
-                    <SectionTitle title='Known For:' />
-
-
-                    <div className="flex overflow-x-auto gap-4 py-4">
-                        {filteredCast.map((movie, i) => (
-                            <div className="w-[200px] min-w-[200px] " key={i}>
-                                <MovieCard movieData={movie} />
-                            </div>
-
-                        ))}
-
-
-                    </div>
-                </div>
-            }
-
-
-
-        </div >
-    )
-}
-
-export default KnownForMovies
+export default KnownForMovies;
